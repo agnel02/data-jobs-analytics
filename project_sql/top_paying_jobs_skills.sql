de27242254926cdfadb4c750f89139bd786f7fcf
Jebas Agnel <agnelmicheal9028@gmail.com>
2026-03-30 22:20:50 +0530

    Added dataset

--- project_sql/top_paying_jobs_skills.sql
+++ project_sql/top_paying_jobs_skills.sql
@@ -1,17 +1,15 @@
 SELECT
   skills,
-  ROUND(AVG(salary_year_avg),2) as avg_salary_paid  
+  ROUND(AVG(salary_year_avg),2) as avg_salary_paid ,ROW_NUMBER()OVER(ORDER BY ROUND(AVG(salary_year_avg),2)DESC) as SR_NO
 FROM job_postings_fact
 INNER JOIN skills_job_dim on job_postings_fact.job_id=skills_job_dim.job_id
 INNER JOIN skills_dim on skills_job_dim.skill_id=skills_dim.skill_id
 WHERE
-  job_title_short ='Data Analyst'
-  AND job_work_from_home= TRUE
+ job_work_from_home= TRUE
 GROUP BY
   skills
 HAVING AVG(salary_year_avg) IS NOT NULL
-ORDER BY
-  AVG(salary_year_avg) DESC
+ORDER BY avg_salary_paid DESC
 LIMIT 50;
 
 ----------------------------------------------------------------------------------------------------------------------
