f929fb522c6e5d368a3a112106d5a7364239e301
Jebas Agnel <agnelmicheal9028@gmail.com>
2026-04-19 21:26:51 +0530

    Add SQL files

--- project_sql/top_paying_jobs_skills.sql
+++ project_sql/top_paying_jobs_skills.sql
@@ -12,38 +12,4 @@ HAVING AVG(salary_year_avg) IS NOT NULL
 ORDER BY avg_salary_paid DESC
 LIMIT 50;
 
-----------------------------------------------------------------------------------------------------------------------
 
--- Select the skill name
--- Calculate the average yearly salary for each skill and round it to 2 decimal places
-SELECT
-  skills,
-  ROUND(AVG(salary_year_avg),2) AS avg_salary_paid
-
--- Main table containing job postings and salary information
-FROM job_postings_fact
-
--- Join table that links jobs with required skills
-INNER JOIN skills_job_dim 
-  ON job_postings_fact.job_id = skills_job_dim.job_id
-
--- Join table that contains the skill names
-INNER JOIN skills_dim 
-  ON skills_job_dim.skill_id = skills_dim.skill_id
-
--- Filter only Data Analyst jobs
--- Filter only remote (work from home) jobs
-WHERE
-  job_title_short = 'Data Analyst'
-  AND job_work_from_home = TRUE
-
--- Group results by skill so we can calculate average salary per skill
-GROUP BY skills
-
--- Sort skills by highest average salary first
-ORDER BY avg_salary_paid DESC
-
--- Show only the top 50 highest paying skills
-LIMIT 50;
-
-------------------------------------------------------------------------------------------------
